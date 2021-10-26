4fc5c052f5b4da72714583b12f0553ea01a168c5
Sasikumar <sasikumar.g@somaiya.edu>
2021-10-26 18:26:06 +0530

    ui of landing, signup and signin

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-   res.render('landing.ejs');
+    res.render('landing.ejs');
+});
+router.get('/signin', (req, res) => {
+    res.render('signin.ejs');
 });
 
 router.get('/profile', (req, res) => {
-   res.render('profile.ejs');
+    res.render('profile.ejs');
 });
 
 router.get('/create_test', (req, res) => {
-   res.render('createTest.ejs');
+    res.render('createTest.ejs');
 });
 
 module.exports = router;
